5cb6653efe67918f3163e2409cd79ae139eae85c
Edgar21AB <134448114+Edgar212004@users.noreply.github.com>
2024-02-24 11:59:21 -0600

    Creacion del proyecto

---
+++ PlantaRecicladora/src/main/java/com/mycompany/plantarecicladora/PlantaRecicladora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.plantarecicladora;
+
+/**
+ *
+ * @author aubog
+ */
+public class PlantaRecicladora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
